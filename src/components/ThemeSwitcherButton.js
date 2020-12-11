c946f4f247ef01a4c4e8becd295dcc1750b3d912
Sandro Bochorishvili <Sanideth@gmail.com>
2020-12-11 14:26:20 +0400

    deleted billion classlist toggles from elements and added css variable switcher if/else statment for document

--- src/components/ThemeSwitcherButton.js
+++ src/components/ThemeSwitcherButton.js
@@ -7,40 +7,17 @@ function ThemeSwitcherButton() {
         //DOM element variables
         const btnCircle = document.getElementById('btn-circle');
         const btnThemeSwitcher = document.getElementById('btn-theme-switcher');
-        const headingPrimary = document.getElementById('heading-primary');
-        const boxGridPrimary = document.querySelectorAll('.main__box-grid-primary');
-        const boxGridSecondary = document.querySelectorAll('.main__box-grid-secondary');
-        const userTextNumbers = document.querySelectorAll('.main__user-text-numbers');
-        const userInteractionNumbers = document.querySelectorAll('.main__user-interaction-numbers');
+    
         
         // Button active state style change
         btnCircle.classList.toggle('btn-circle-active');
         btnThemeSwitcher.classList.toggle('btn-active');
-        document.body.classList.toggle('body-active');
-        headingPrimary.classList.toggle('text-color-darkest');
-        //User numbers color change on active state
-        userTextNumbers.forEach(element => {
-            element.classList.toggle('text-color-darkest');
-        })
-
-        //Grid box style and hover state change on active state
-        boxGridPrimary.forEach(element => {
-            element.classList.toggle('bg-color-lighten');
-            element.classList.toggle('box-grid-active');
-        })
-
-        boxGridSecondary.forEach(element => {
-            element.classList.toggle('bg-color-lighten')
-            element.classList.toggle('box-grid-active');
-        })
-        // User interaction numbers style change on active state
-        userInteractionNumbers.forEach(element => {
-            element.classList.toggle('text-color-darkest');
-        })
-        
-        
-
-        
+        // Css variables change to light mode and back
+        if (btnThemeSwitcher.classList.contains('btn-active')) {
+            document.documentElement.setAttribute('data-theme', 'light');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
     }
     return (
         <div className="button-container">
